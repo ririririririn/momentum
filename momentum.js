5c18bbb8f81e6e115e62ec1df5780b99cb0c87b1
ririririririn <123622271+ririririririn@users.noreply.github.com>
2023-03-22 23:36:38 +0900

    23.03.22
    login - main 이동

---
+++ momentum.js
@@ -0,0 +1,17 @@
+let inputId = document.querySelector('.userid')
+
+
+let btnLogin = document.querySelector('.btnLogin')
+
+
+
+btnLogin.addEventListener('click',login)
+
+function login(){
+  loginSucess()
+  // inputId.value =/
+
+}
+
+function loginSucess(){
+location.href = "./momentum-main.html";}
